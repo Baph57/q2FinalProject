5533531ff93ed0c200f1c1611997b41d76caf2f1
Marko Pecic <mpecic2010@gmail.com>
2018-11-27 12:18:48 -0700

    created home.ejs, some styling and forms created for home, login, reg

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,12 @@
 //Update the name of the controller below and rename the file.
 const activities = require("../controllers/activities.js")
 module.exports = function(app){
-  app.get('/', activities.login);
-  app.get('/register', activities.register)
+  app.get('/', activities.home);
+  app.get('/login', activities.login);
+  app.get('/register', activities.register);
   app.get('/landing:id', activities.landing);
   app.get('/profile:id', activities.profile);
   app.get('/create', activities.create);
   app.get('/delete:id', activities.delete);
-  app.get('/edit:id', activities.edit);
+  app.get('/edit:id', activities.edit)
 }
